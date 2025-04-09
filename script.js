cefcb85211e3fd9d8a0778201032891e9e33dce8
Diana Bernabei <bernabei.d@gmail.com>
2025-04-09 10:26:09 +0200

    feat: udpated a11y bug for select

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ function getColorClass(hexColor) {
 }
 
 const iconList = `<svg aria-hidden="true" class="list-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="8" cy="5" r="3" stroke="#000000" stroke-width="1.5"/>
-    <path d="M3 14C3 11.2386 5.23858 9 8 9C10.7614 9 13 11.2386 13 14" stroke="#000000" stroke-width="1.5"/>
+    <circle cx="8" cy="5" r="3" stroke="#333333" stroke-width="1.5"/>
+    <path d="M3 14C3 11.2386 5.23858 9 8 9C10.7614 9 13 11.2386 13 14" stroke="#333333" stroke-width="1.5"/>
 </svg>`;
 
 function createSpeakersList(speakers) {
